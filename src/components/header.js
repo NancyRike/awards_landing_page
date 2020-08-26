85282fe6e5bc943d5e5dea5c538d9c29759f2362
unknown <nancyrike7@gmail.com>
2020-08-26 03:10:45 +0100

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header (){
+    return(
+        <div>
+            <a href="">ENGLISH</a>
+            <h6>the awards of design, creativity and innovation on the internet</h6>
+            
+            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-justify" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
+</svg>
+        </div>
+    )
+}
+export default Header;
